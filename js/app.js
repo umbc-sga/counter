8dd6dbd19f48bcc97fa6062433222d8c626886e0
Rees Draminski <rees.draminski@gmail.com>
2022-05-20 17:39:01 -0400

    save to localstorage

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 let count = 0;
+const hasLocalStorage = checkForLocalStorage();
 
 (function initUI() {
     bindButtons();
 
+    if (hasLocalStorage) 
+    {
+        let value = localStorage.getItem("counter");
+        if (!isNaN(value))
+        {
+            count = value;
+        }
+    }
+
     updateCounter();
 })();
-
-
 
 function bindButtons() {
     document.getElementById("upButton").onclick = () => {
@@ -27,4 +35,27 @@ function bindButtons() {
 
 function updateCounter() {
     document.getElementById("counter").innerText = count;
+
+    if (hasLocalStorage)
+    {
+        localStorage.setItem("counter", count);
+    }
+}
+
+/**
+ * Check if localStorage exist and is available.
+ * https://stackoverflow.com/questions/16427636/check-if-localstorage-is-available/16427747
+ * @return {Boolean} localStorageExists
+ */
+ function checkForLocalStorage(){
+    const test = "test";
+
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+
+        return true;
+    } catch(e) {
+        return false;
+    }
 }
